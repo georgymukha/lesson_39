6595a38f67506c7f123f2771fa988d1a6bae16fd
Alex Som <alex.som86@gmail.com>
2021-09-11 18:50:35 +0600

    change main component

--- src/containers/PizzaBuilder/PizzaBuilder.js
+++ src/containers/PizzaBuilder/PizzaBuilder.js
@@ -1,17 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import './PizzaBuilder.css';
 
-class PizzaBuilder extends Component {
-
-  render() {
-    return (
-        <div className="pizza-wrap">
-          <h1>Pizza here</h1>
-          <h3>Controls here</h3>
-        </div>
-    );
-  }
+function PizzaBuilder() {
+  return (
+    <div className="pizza-wrap">
+      <h1>Pizza here</h1>
+      <h3>Controls here</h3>
+    </div>
+  );
 }
 
-export default PizzaBuilder;
+export default PizzaBuilder
