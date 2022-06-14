b50472027928e6721cf6effacf506f25d430a53e
Alex Som <alex.som86@gmail.com>
2022-06-14 19:14:17 +0600

    add useEffect

--- src/containers/Orders/Orders.jsx
+++ src/containers/Orders/Orders.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useEffect } from 'react';
+
+import axios from '../../api/axios-order';
 
 function Orders() {
+
+  useEffect(() => {
+
+  }, [])
+
   return (
     <div>Orders</div>
   )
