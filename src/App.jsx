8d2c1e8a113c68528cb78186b7c03f2e981034f5
Alex Som <alex.som86@gmail.com>
2022-06-14 19:07:50 +0600

    add orders

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,23 @@ import Layout from "./components/Layout/Layout";
 import {Routes, Route} from 'react-router-dom';
 
 import './App.css';
+import Orders from "./containers/Orders/Orders";
 
 function App() {
   return (
     <>
-     <Layout>
+      <Layout>
         <Routes>
-          <Route path="/" element={<PizzaBuilder/>} />
-          <Route path="/checkout" element={<Checkout/>}>
-            <Route path="contact-data" element={<ContactData/>} />
+          <Route path="/" element={<PizzaBuilder />} />
+          <Route path="/checkout" element={<Checkout />}>
+            <Route path="contact-data" element={<ContactData />} />
           </Route>
-          <Route path="*" element={<h1>404</h1>}/>
+          <Route path="/orders" element={<Orders/>} />
+          <Route path="*" element={<h1>404</h1>} />
         </Routes>
-     </Layout>
+      </Layout>
     </>
-  )
+  );
 }
 
 export default App;
